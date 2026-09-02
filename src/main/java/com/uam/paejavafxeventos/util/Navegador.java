ecf46569c4db4d757c8c3bad942a8146d18cd0cf
FCuadra-JAG <f.cuadra@uamv.edu.ni>
2026-09-02 00:08:31 -0600

    declarar variables de animacion

--- src/main/java/com/uam/paejavafxeventos/util/Navegador.java
+++ src/main/java/com/uam/paejavafxeventos/util/Navegador.java
@@ -1,4 +1,22 @@
+
 package com.uam.paejavafxeventos.util;
 
+import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.util.Duration;
+
+import java.io.IOException;
+
+//Utilidad reutilizable para cambiar de pantalla CON una animación de * fade (desvanecido), en vez de un cambio brusco.//
+
+
 public class Navegador {
-}
+    private static final Duration DURACION = Duration.millis(250);
+
+    public static void cambiarEscenaConFade(ActionEvent event, String rutaFxml) {
+
+    }
