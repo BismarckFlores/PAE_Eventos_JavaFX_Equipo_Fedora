32030b22efcba8ed083122d64b2313405eb401a0
FCuadra-JAG <f.cuadra@uamv.edu.ni>
2026-09-01 23:39:11 -0600

    dar permiso a JavaFX de acceder a PulperiaController

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.uam.paejavafxeventos {
 
     opens com.uam.paejavafxeventos to javafx.fxml;
     exports com.uam.paejavafxeventos;
+
+    opens com.uam.paejavafxeventos.controller to javafx.fxml;
 }
