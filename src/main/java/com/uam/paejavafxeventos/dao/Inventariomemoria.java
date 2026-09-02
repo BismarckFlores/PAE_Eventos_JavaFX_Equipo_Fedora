4184cc0ddf42c8edb90498bdfad287ee08681852
FCuadra-JAG <f.cuadra@uamv.edu.ni>
2026-09-02 00:31:39 -0600

    declaracion de la instancia

--- src/main/java/com/uam/paejavafxeventos/dao/Inventariomemoria.java
+++ src/main/java/com/uam/paejavafxeventos/dao/Inventariomemoria.java
@@ -1,4 +1,15 @@
 package com.uam.paejavafxeventos.dao;
 
+import com.uam.paejavafxeventos.model.Producto;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Inventariomemoria {
+    // la funcion de esta clase es guardar el inventario atraves de todas las pantallas
+
+    // la unica instancia que va a exister en el programa
+    private static final Inventariomemoria instancia = new Inventariomemoria();
+
+    private final Map<String, Producto> productos = new HashMap<>();
 }
